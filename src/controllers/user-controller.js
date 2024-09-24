7b19f1bd51a1184ccce1b41b418d404171d9d2c7
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-09-24 11:39:17 -0300

    autenticação

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,31 @@
+import User from "../models/user-model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+      nickname: req.body.nickname,
+      role: req.body.role,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    }).exec();
+    if (!user || !(await !user.isValidPassword(rq.body.password))) {
+      res.status(404).json({
+        error: "Email os passaword incorrect",
+      });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
---
+++ src/routes/movie-route.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { store, index } from "../controllers/movie-controller.js";
+const router = Router();
+router.post("/", store);
+router.get("/", index);
+export default router;
